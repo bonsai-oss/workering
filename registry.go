2dd110c8699e0baf415c8d7c317b3054c6bbce26
Florian Bauer <florian@fsrv.xyz>
2022-12-03 22:10:16 +0100

    feat!: use RegisterSet type for worker registration

--- registry.go
+++ registry.go
@@ -7,6 +7,11 @@ import (
 
 type Worker func(ctx context.Context, done chan<- any)
 
+type RegisterSet struct {
+	Name   string
+	Worker Worker
+}
+
 var workers map[string]Worker
 var status map[*Worker]WorkerStatus
 var contexts map[*Worker]context.Context
@@ -21,16 +26,22 @@ func init() {
 	cancelFuncs = make(map[*Worker]context.CancelFunc)
 }
 
-func Register(name string, worker Worker) {
-	if workers == nil {
-		workers = make(map[string]Worker)
+func Register(sets ...RegisterSet) {
+	mux.Lock()
+	defer mux.Unlock()
+	for _, set := range sets {
+		if set.Name == "" {
+			panic("worker name must not be empty")
+		}
+		if set.Worker == nil {
+			panic("worker must not be nil")
+		}
+		if _, ok := workers[set.Name]; ok {
+			panic("worker already registered")
+		}
+		workers[set.Name] = set.Worker
+		status[&set.Worker] = Stopped
 	}
-	if _, ok := workers[name]; ok {
-		panic("worker already registered")
-	}
-	workers[name] = worker
-	status[&worker] = Stopped
-
 }
 
 func Get(name string) Worker {
